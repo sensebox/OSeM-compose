12e7b59c7cc040360338652bed1795680244703f
cloudmanager <cloudmanager@giw-whitera>
2017-02-23 17:52:59 +0100

    add volume creation script

---
+++ create-volumes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+volumes="usersketches,userimages,caddy-data,frontend-assets,mongo-data"
+
+IFS=$','
+for volume_name in $volumes; do
+  echo -n "Creating docker volume "
+  docker volume create -d local "$volume_name"
+  echo "done"
+done
